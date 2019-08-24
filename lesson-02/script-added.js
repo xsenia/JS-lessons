eeff8319eca615d0204a50daa4b4f1c8407beb4c
xsenia <belkinasan@yandex.ru>
2019-08-24 22:55:53 +0500

    Доделаное второе задание

--- lesson-02/script-added.js
+++ lesson-02/script-added.js
@@ -6,9 +6,20 @@ let num = 266219,
 symbols = num.toString();                        //привели к строке
 symbols = symbols.split('');                     //разбили строку на массив
 
-for(let i=0; i<symbols.length; i++){
+for (let i=0; i<symbols.length; i++){
     number = Number(symbols[i]);                 //привели к числу
     result = result * number;
 }
 
 console.log('Результат: ', result);              // результат
+
+
+const a = result;
+for (let i=1; i<3; i++){
+    result = a * result;
+}
+console.log('Результат в степени: ', result);
+
+let sliceStr = result.toString();
+sliceStr = sliceStr.slice(0,2);
+console.log('Первые два числа: ', sliceStr);
