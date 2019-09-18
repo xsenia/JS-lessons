172281e8100ecdddd4a3761d2fb17ba78e0c9a8d
xsenia <belkinasan@yandex.ru>
2019-09-19 00:15:58 +0500

    slick slider

--- lesson-14-practice/script/script.js
+++ lesson-14-practice/script/script.js
@@ -337,6 +337,133 @@ window.addEventListener('DOMContentLoaded', function() {
 
     slider();
 
+    //slider carousel
+
+    
+
+    class sliderCarousel {
+        constructor({
+            main, 
+            wrap, 
+            next,
+            prev,
+            infinity = false,
+            position = 0,
+            slidesToShow = 3
+        }) {
+            this.main = document.querySelector(main);
+            this.wrap = document.querySelector(wrap);
+            this.slides = document.querySelector(wrap).children;
+            this.next = document.querySelector(next);
+            this.prev = document.querySelector(prev);
+            this.slidesToShow = slidesToShow;
+            this.options = {
+                position,
+                infinity,
+                widthSlide: Math.floor(100/this.slidesToShow)
+            };
+        }
+
+        init(){
+            //console.log(this.slides);
+            this.addGloClass();
+            this.addStyle();
+            if(this.prev && this.next) {
+                this.controlSlider();
+            } else {
+                this.addArrow();
+                this.controlSlider();
+            }
+        }
+
+        addGloClass() {
+            this.main.classList.add('glo-slider');
+            this.wrap.classList.add('glo-slider__wrap');
+            for (const item of this.slides) {
+                item.classList.add('glo-slider__item');
+            }
+        }
+
+        addStyle() {
+            const style = document.createElement('style');
+            style.id = 'sliderCarousel-style';
+            style.textContent = `
+                .glo-slider {
+                    overflow: hidden;
+                }
+                .glo-slider__wrap {
+                    display: flex;
+                    transition: transform .5s;
+                    will-change: transform;
+                }
+                .glo-slider__item {
+                    flex: 0 0 ${this.options.widthSlide}%;
+                    margin: auto 0;
+                }
+                .companies .companies-hor-item {
+                    margin: 0;
+                }
+            `;
+            document.head.appendChild(style);
+        }   
+
+        controlSlider() {
+            this.prev.addEventListener('click', this.prevSlider.bind(this));
+            this.next.addEventListener('click', this.nextSlider.bind(this));
+        }
+
+        prevSlider() {
+            if (this.options.infinity || this.options.position > 0) {
+                --this.options.position;
+                console.log('this.options.position: ', this.options.position);
+                if (this.options.position < 0) {
+                    this.options.position = this.slides.length - this.slidesToShow;
+                }
+                this.wrap.style.transform = `translateX(-${this.options.position * this.options.widthSlide}%)`;
+            }
+            
+        }
+
+        nextSlider() {
+            if (this.options.infinity || this.options.position < this.slides.length - this.slidesToShow) {
+                ++this.options.position;
+                console.log('this.options.position: ', this.options.position);
+                
+                if(this.options.position > this.slides.length - this.slidesToShow) {
+                    this.options.position = 0;
+                } 
+                this.wrap.style.transform = `translateX(-${this.options.position * this.options.widthSlide}%)`;
+            }
+        }
+
+        addArrow() {
+            this.prev = document.createElement('button');
+            this.next = document.createElement('button');
+
+            this.prev.className = 'glo-slider__prev';
+            this.next.className = 'glo-slider__next';
+
+            this.main.appendChild(this.prev);
+            this.main.appendChild(this.next);
+
+        }
+
+
+    }
+
+
+    const options = {
+        main: '.companies-wrapper',
+        wrap: '.companies-hor',
+        prev: '#test-left',
+        next: '#test-right',
+        slidesToShow: 4,
+        infinity: true
+    }
+    const carousel = new sliderCarousel(options);
+    carousel.init();
+
+
 
 
 });
